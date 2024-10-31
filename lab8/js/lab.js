ae660f1cb75a25d1fe70f4c198e7b6708e8f5a92
Trevor Chee <08trevor16@gmail.com>
2024-10-30 19:11:05 -0700

    lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,20 +1,37 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// File: lab.js
+// Author: Kimberly Estrada & Trevor Chee <kestrada9@ucsc.edu> <tlchee@ucsc.edu>
+// Date: 31.10.2024
+// License: Public Domain
+// Purpose: Experiment with anonymous functions and callbacks
+
+// Description:
 
 // Constants
+let myArray = [2, 3, 5, 7, 9, 11, 12, 16, 18];
+
+let alteredArray = myArray.map(multiplyTwo);
+
+let isGreaterOrEqual20 = alteredArray.map(function (x) {
+  return x >= 20;
+});
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// multipleTwo is a function that multiplies numbers by 2
+function multiplyTwo(x) {
+  let results = x * 2;
+  return results;
 }
 
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
+  console.log("Taking an array and multiplying each of its elements by 2.");
+  console.log("Original Array: ", myArray);
+  console.log("Altered Array: ", myArray.map(multiplyTwo));
+  console.log("Is each element in the Altered Array >= 20?");
+  console.log(
+    "Well let's check... Checking if each element is >= 20: ",
+    isGreaterOrEqual20
+  );
 }
 
 // let's get this party started
