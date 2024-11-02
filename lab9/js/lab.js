98cc28f4f02569a9034aebaf0f61b8cd9c67da29
Trevor Chee <08trevor16@gmail.com>
2024-11-01 18:08:09 -0700

    lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,16 +1,53 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// File: lab.js
+// Author: Kimberly Estrada & Trevor Chee <kestrada9@ucsc.edu> <tlchee@ucsc.edu>
+// Date: 4.11.2024
+// License: Public Domain
+// Purpose: Experiment with jQuery
 
-// Constants
+// Description:
 
-// Functions
+// jQuery
+//  add button to challenge section
+// $("#challenge").append("<button id='button-challenge'>Make Special</button>");
+//  add a click listener for the challenge button
+// $("#button-challenge").click(function () {
+//   add (or subtract) the "special class to the section"
+//   $("#challenge").toggleClass("special");
+// });
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//  add button to problems section
+// $("#problems").append("<button id='button-problems'>Make Special</button>");
+//  add a click listener for the problems button
+// $("#button-problems").click(function () {
+//   add (or subtract) the "special class to the section"
+//   $("#problems").toggleClass("special");
+// });
+
+//  add button to reflection section
+// $("#reflection").append("<button id='button-reflection'>Make Special</button>");
+//  add a click listener for the results button
+// $("#button-reflection").click(function () {
+//   add (or subtract) the "special class to the section"
+//   $("#reflection").toggleClass("special");
+// });
+
+//  add button to results section
+// $("#results").append("<button id='button-results'>Make Special</button>");
+//  add a click listener for the results button
+// $("#button-results").click(function () {
+//   add (or subtract) the "special class to the section"
+//   $("#results").toggleClass("special");
+// });
+
+// Task X
+// add a button to the challenge, problems, reflection, and results sections
+$(".special-section").append(
+  "<button class='special-button'>Make Special</button>"
+);
+$(".special-button").click(function () {
+  // add (or subtract) the "special class to the section"
+  $(this).parent().toggleClass("special");
+});
 
 function main() {
   console.log("Main function started.");
