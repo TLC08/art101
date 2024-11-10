0d0b37b0a3f0d428fcd42d6ceffec3bf69de72a7
Trevor Chee <08trevor16@gmail.com>
2024-11-09 17:21:24 -0800

    lab 11.a

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,16 +1,33 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// File: lab.js
+// Author: Kimberly Estrada & Trevor Chee <kestrada9@ucsc.edu> <tlchee@ucsc.edu>
+// Date: 11.11.2024
+// License: Public Domain
+// Purpose: Experiment with JavaScript Events and Forms
 
-// Constants
+// Description: This JavaScript/jQuery script gets a value from an input field and outputs a modified version.
+
+// Constants 
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split("").sort().join("");
 }
+
+// click listener for button
+$("#submit").click(function(){
+});
+
+// get value of input field
+const userName = $("#user-name").val();
+
+// now let's sort it
+userNameSorted = sortString(userName);
+
+// append a new div to our output div
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
 
 function main() {
   console.log("Main function started.");
