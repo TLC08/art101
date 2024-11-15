71cf516612dc4413d22aa5185a23f911cef28fb9
Trevor Chee <08trevor16@gmail.com>
2024-11-15 01:03:03 -0800

    lab 13 final

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -6,56 +6,75 @@
 
 // Description: This JavaScript/jQuery script creates a loop function.
 
+// Constants
+
+const maxFactors = 4;
+
 // Functions
 
-// Loops through numbers 1 to 200, listing them as you go and will return "Fizz", "Buzz", "Boom", 
-// or a combination of the 3 depending on whether or not the number is a multiple of 3, 5, and/or 7 respectively.
-// Made with the help of ChatGPT
-function fizzBuzzBoom() {
-  const resultsContainer = $("#output"); // Get the results container
-  let columnCount = 0; // Counter to keep track of when to start a new column
-  let columnContainer; // Variable to store column div
-  
-  for (let i = 0; i <= 200; i++) {
-    let message = `${i}: `;
-    
-    // Create a variable to build the message based on multiples of 3, 5, and 7
-    let fizzMessage = "";
-
-    if (i % 3 === 0) {
-      fizzMessage += "Fizz";
+// Get the values from the webpage and write them in an object
+// This expects to have input fields with ids num0, text0, num1, text1, etc
+// Returns an object that looks like this:
+// {3: "Fizz", 5: "Buzz", 7: "Boom"}
+// Made with the help of Wes Modes
+function getFactorObj() {
+  const factorObj = {};
+  for (let factor = 0; factor < maxFactors; factor++) {
+    let numId = "num" + factor;
+    let textId = "text" + factor;
+    let numValue = $("#" + numId).val();
+    let textValue = $("#" + textId).val();
+    console.log(factor + ") num:", numValue, "text:", textValue);
+    // if either value is blank, don't use it
+    if (numValue && textValue) {
+      factorObj[numValue] = textValue;
     }
-    if (i % 5 === 0) {
-      fizzMessage += "Buzz";
-    }
-    if (i % 7 === 0) {
-      fizzMessage += "Boom";
-    }
-    
-    if (fizzMessage) {
-      message += fizzMessage + "!"; // Add "!" only once if there are multiples
-    } else {
-      message += ""; // If no multiples found, default message
-    }
-    
-    // If it's the first item or we need to start a new column (every 17 items)
-    if (columnCount % 17 == 0) {
-      // Create a new column div and append it to results container
-      columnContainer = $('<div class="column"></div>');
-      resultsContainer.append(columnContainer); // Append the column to the container
-    }
-
-    //Append the message into the correct column
-    columnContainer.append('<p>' + message + '</p>');
-    
-    columnCount++; // Increment the counter to track the number of results
   }
-
-  // Clear any floeated elements after the columns
-  resultsContainer.append('<div class="clearfix"></div>');
+  return factorObj;
 }
 
-// Call the function to populate the results when the page loads
-$(document).ready(function() {
-  fizzBuzzBoom();
+// Loops over the numbers and outputs the number and matching text for factors
+// Made with the help of Wes Modes and ChatGPT
+function fizzBuzzBoom(maxNums, factorObj) {
+  for (let i = 0; i <= maxNums; i++) {
+    // Creates a variable to build the message based on multiples of the factors
+    let message = `${i}: `;
+
+    // Iterates over all of the output numbers
+    for (let factor in factorObj) {
+      // Checks to see if this num is a multiple of factor
+      if (i % factor == 0) {
+        // If yes, than add the text to the message
+        message += factorObj[factor];
+      }
+    }
+    if (message !== `${i}: `) {
+      // If the message doesn't strictly equal `${i}: `
+      message += "!"; // Add "!" only once if there are multiples
+    }
+    // Appends number and output
+    $("#output").append("<p>" + message + "</p>");
+  }
+}
+
+function reportError(str) {
+  $("#output").append("<div class='error'>" + str + "</div");
+}
+
+// A button to run the program
+$("#button").click(function () {
+  let max = $("#max").val();
+  if (!max) {
+    reportError("You must provide a maximum");
+    return;
+  }
+  // Calls the getFactorObj() function and assigns it to a variable
+  let factorObj = getFactorObj();
+  console.log("factorObj:", factorObj);
+  if (Object.keys(factorObj).length === 0) {
+    reportError("You must provide at least one factor and text");
+    return;
+  }
+  // Calls the fizzBuzzBoom() function
+  fizzBuzzBoom(max, factorObj);
 });
